b38d641d6b6688bc88ad0768f81ea38513e5a066
Nawaf Kamal <xp.mo91@gmail.com>
2025-06-25 14:51:43 +0300

    done

---
+++ bonus.py
@@ -0,0 +1,6 @@
+import art
+from colorama import Fore
+
+print(Fore.GREEN)
+art.tprint("TRAIN MAKE YOU STRONG",font='butterfly')
+art.tprint("THAT IT",font='sub-zero')
---
+++ main.py
@@ -0,0 +1,6 @@
+import art
+
+art.tprint("BELIEVE AND ACHEIVE",font='block')
+
+
+art.tprint("Hello",font='sub-zero')
